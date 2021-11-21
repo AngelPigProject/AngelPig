c736f2780fd924e21f49dd8d593dbbfa6b5ce97a
Angel Pig <94281869+AngelPigProject@users.noreply.github.com>
2021-11-21 10:01:52 +0300

    Update contract content

--- migrations/2_delpoy_contracts.js
+++ migrations/2_delpoy_contracts.js
@@ -1,18 +1,7 @@
 const Factory = artifacts.require("UniswapV2Factory");
-const Token1 = artifacts.require("Token1");
-const Token2 = artifacts.require("Token2");
+const Token1 = artifacts.require("APGToken");
+const Token2 = artifacts.require("APGBank");
 const MyContract=artifacts.require("MyContract")
 module.exports = async function (deployer,network,addresses) {
-    let token1Address,token2Address;
-    await deployer.deploy(MyContract);
-    await deployer.deploy(Token1);
-    const token1 = await Token1.deployed();
-    await deployer.deploy(Token2);
-    const token2 = await Token2.deployed();
-    token1Address = token1.address;
-    token2Address = token2.address;
-    if (network=='mainnet'){}else {
-        await deployer.deploy(Factory, addresses[0]);
-        const factory = await Factory.deployed();
-        factory.createPair(token1Address,token2Address);}
+    //
 };
